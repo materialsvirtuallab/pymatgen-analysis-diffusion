f6fcb67f6081edfb35c1b1ace432cdd7546459f6
__sp__ <ongsp@eng.ucsd.edu>
2016-01-15 12:16:54 -0800

    Fix README.

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # Pymatgen-diffusion
 
 This is an add-on to pymatgen for diffusion analysis that is developed
-by the Materials Virtual Lab.
+by the Materials Virtual Lab. Note that it relies on pymatgen for structural 
+manipulations, file io, and preliminary analyses. In particular, the 
+pymatgen.analysis.diffusion_analyzer is used heavily. The purpose of this add-on
+is to provide other diffusion analyses, using trajectories extracted using the
+DiffusionAnalyzer class.
+
+# Features (non-exhaustive!)
+
+1. Van-Hove analysis
+2. Probability density
+3. Clustering (e.g., k-means with periodic boundary conditions).
 
 # Acknowledgements
 
-This code is funded by the National Science Foundation’s 
-Designing Materials to Revolutionize and Engineer our Future 
-(DMREF) program under Grant No. 1436976.
+This code is funded by the National Science Foundation’s Designing Materials
+to Revolutionize and Engineer our Future (DMREF) program under Grant No. 
+1436976.
--- pymatgen_diffusion/aimd/van_hove.py
+++ pymatgen_diffusion/aimd/van_hove.py
@@ -12,34 +12,37 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 # TODO: add unittests, and ipython notebook examples
 
+
 def gen_gaussian(x, mu, sig):
-    return np.exp(-np.power(x - mu, 2.0)/2.0/np.power(sig,2.0))/np.sqrt(2.0*np.pi)/sig
+    return np.exp(-np.power(x - mu, 2) / 2 / np.power(sig, 2)) / np.sqrt(
+        2 * np.pi) / sig
+
 
 class VanHoveAnalysis(object):
-
     """
-        Class for van Hove function analysis. In particular, self-part (Gs) and
-        distinct-part (Gd) of the van Hove correlation function G(r,t)
-        for given species and given structure are computed. If you use this class,
-        please consider citing the following paper:
+    Class for van Hove function analysis. In particular, self-part (Gs) and
+    distinct-part (Gd) of the van Hove correlation function G(r,t)
+    for given species and given structure are computed. If you use this class,
+    please consider citing the following paper:
 
-        Zhu, Z.; Chu, I.-H.; Deng, Z. and Ong, S. P. "Role of Na+ Interstitials and Dopants
-        in Enhancing the Na+ Conductivity of the Cubic Na3PS4 Superionic Conductor".
-        Chem. Mater. (2015), 27, pp 8318–8325
+    Zhu, Z.; Chu, I.-H.; Deng, Z. and Ong, S. P. "Role of Na+ Interstitials
+    and Dopants in Enhancing the Na+ Conductivity of the Cubic Na3PS4
+    Superionic Conductor". Chem. Mater. (2015), 27, pp 8318–8325
     """
 
-
-    def __init__(self, obj, avg_nsteps=50, Ntot=101, rmax=10.0, step_skip=50, sigma=0.1,
-               species = ["Li","Na"]):
+    def __init__(self, pmg_diff_analyzer, avg_nsteps=50, ngrid=101, rmax=10.0,
+                 step_skip=50, sigma=0.1, species=["Li", "Na"]):
         """
         Initization.
 
         Args:
-            obj (DiffusionAnalyzer): A DiffusionAnalyzer object
+            pmg_diff_analyzer (DiffusionAnalyzer): A
+                pymatgen.analysis.diffusion_analyzer.DiffusionAnalyzer object
             avg_nsteps (int): Number of t0 used for statistical average
-            Ntot (int): Number of radial grid points
+            ngrid (int): Number of radial grid points
             rmax (float): Maximum of radial grid (the minimum is always set zero)
             step_skip (int): # of time steps skipped during analysis. It defines
                         the resolution of the reduced time grid
@@ -47,34 +50,34 @@ class VanHoveAnalysis(object):
             species ([string]): a list of specie symbols of interest
         """
 
-        #initial check
-        if step_skip <=0:
+        # initial check
+        if step_skip <= 0:
             raise ValueError("skip_step should be >=1!")
 
-        nions, nsteps, ndim = obj.disp.shape
+        nions, nsteps, ndim = pmg_diff_analyzer.disp.shape
 
         if nsteps <= avg_nsteps:
             raise ValueError("Number of timesteps is too small!")
 
         ntsteps = nsteps - avg_nsteps
 
-        if Ntot - 1 <= 0:
+        if ngrid - 1 <= 0:
             raise ValueError("Ntot should be greater than 1!")
 
         if sigma <= 0.0:
             raise ValueError("sigma should be > 0!")
 
-        dr = rmax / (Ntot-1)
-        interval = np.linspace(0.0, rmax, Ntot)
-        aux_factor = np.zeros_like(interval, dtype = np.double)
-        gaussians = np.zeros((Ntot, Ntot), dtype = np.double)
-        reduced_nt = int(ntsteps/float(step_skip)) + 1
+        dr = rmax / (ngrid - 1)
+        interval = np.linspace(0.0, rmax, ngrid)
+        aux_factor = np.zeros_like(interval, dtype=np.double)
+        gaussians = np.zeros((ngrid, ngrid), dtype=np.double)
+        reduced_nt = int(ntsteps / float(step_skip)) + 1
 
         # reduced time grid
         rtgrid = np.arange(0.0, reduced_nt)
         # van Hove functions
-        gsrt = np.zeros((reduced_nt, Ntot), dtype = np.double)
-        gdrt = np.zeros((reduced_nt, Ntot), dtype = np.double)
+        gsrt = np.zeros((reduced_nt, ngrid), dtype=np.double)
+        gdrt = np.zeros((reduced_nt, ngrid), dtype=np.double)
 
         tracking_ions = []
 
@@ -85,71 +88,78 @@ class VanHoveAnalysis(object):
             else:
                 aux_factor[indx] = 4.0 * np.pi * v ** 2
 
-        for i, ss in enumerate(obj.get_drift_corrected_structures()):
+        for i, ss in enumerate(pmg_diff_analyzer.get_drift_corrected_structures()):
             if i == 0:
                 lattice = ss.lattice
-                indices = [j for j, site in enumerate(ss) if site.specie.symbol in species]
+                indices = [j for j, site in enumerate(ss) if
+                           site.specie.symbol in species]
                 rho = float(len(indices)) / ss.lattice.volume
 
             all_fcoords = np.array(ss.frac_coords)
-            tracking_ions.append(all_fcoords[indices,:])
+            tracking_ions.append(all_fcoords[indices, :])
 
         tracking_ions = np.array(tracking_ions)
 
         for indx, y in enumerate(interval):
-            gaussians[indx,:] = gen_gaussian(interval, interval[indx], sigma) / float(avg_nsteps) / \
-                            float(len(indices))
+            gaussians[indx, :] = gen_gaussian(interval, interval[indx],
+                                              sigma) / float(avg_nsteps) / \
+                                 float(len(indices))
 
         # calculate self part of van Hove function
-        image=np.array([0,0,0])
+        image = np.array([0, 0, 0])
         for it in range(reduced_nt):
             dns = Counter()
-            it0 = min(it*step_skip, ntsteps)
+            it0 = min(it * step_skip, ntsteps)
             for it1 in range(avg_nsteps):
                 dists = [lattice.get_distance_and_image(tracking_ions[it1][u],
-                        tracking_ions[it0+it1][u],
-                        jimage=image)[0] for u in range(len(indices))]
+                                                        tracking_ions[
+                                                            it0 + it1][u],
+                                                        jimage=image)[0] for u
+                         in range(len(indices))]
                 dists = filter(lambda e: e < rmax, dists)
 
-                r_indices = [int(dist/dr) for dist in dists]
+                r_indices = [int(dist / dr) for dist in dists]
                 dns.update(r_indices)
 
-            for indx, dn in dns.most_common(Ntot):
-                gsrt[it,:] += gaussians[indx, :] * dn
+            for indx, dn in dns.most_common(ngrid):
+                gsrt[it, :] += gaussians[indx, :] * dn
 
         # calculate distinct part of van Hove function of species
         r = np.arange(-1, 2)
         arange = r[:, None] * np.array([1, 0, 0])[None, :]
         brange = r[:, None] * np.array([0, 1, 0])[None, :]
         crange = r[:, None] * np.array([0, 0, 1])[None, :]
-        images = arange[:, None, None] + brange[None, :, None] +crange[None, None, :]
+        images = arange[:, None, None] + brange[None, :, None] + crange[None,
+                                                                 None, :]
         images = images.reshape((27, 3))
 
         # find the zero image vector
-        zd = np.sum(images**2,axis=1)
+        zd = np.sum(images ** 2, axis=1)
         indx0 = np.argmin(zd)
 
         for it in range(reduced_nt):
             dns = Counter()
-            it0 = min(it*step_skip, ntsteps)
-            #print it + 1, reduced_nt
+            it0 = min(it * step_skip, ntsteps)
+            # print it + 1, reduced_nt
 
             for it1 in range(avg_nsteps):
-                dcf = tracking_ions[it0+it1,:,None,None,:] + images[None,None,:,:] \
-                    - tracking_ions[it1,None,:,None,:]
+                dcf = tracking_ions[it0 + it1, :, None, None, :] + images[None,
+                                                                   None, :, :] \
+                      - tracking_ions[it1, None, :, None, :]
                 dcc = lattice.get_cartesian_coords(dcf)
                 d2 = np.sum(dcc ** 2, axis=3)
-                dists = [d2[u,v,j] ** 0.5 for u in range(len(indices)) for v in range(len(indices)) \
-                    for j in range(27) if u != v or j != indx0]
+                dists = [d2[u, v, j] ** 0.5 for u in range(len(indices)) for v
+                         in range(len(indices)) \
+                         for j in range(27) if u != v or j != indx0]
                 dists = filter(lambda e: e < rmax, dists)
 
-                r_indices = [int(dist/dr) for dist in dists]
+                r_indices = [int(dist / dr) for dist in dists]
                 dns.update(r_indices)
 
-            for indx, dn in dns.most_common(Ntot):
-                gdrt[it,:] += gaussians[indx,:] * dn / aux_factor[indx] / rho
+            for indx, dn in dns.most_common(ngrid):
+                gdrt[it, :] += gaussians[indx, :] * dn / aux_factor[indx] / rho
 
-        self.obj = obj
+        self.obj = pmg_diff_analyzer
         self.avg_nsteps = avg_nsteps
         self.step_skip = step_skip
         self.rtgrid = rtgrid
@@ -157,15 +167,17 @@ class VanHoveAnalysis(object):
         self.gsrt = gsrt
         self.gdrt = gdrt
 
-    def get_gs_plots(self, figsize=(12,8)):
+    def get_gs_plots(self, figsize=(12, 8)):
         """
         Plot self-part van Hove functions.
         """
 
-        y = np.arange(np.shape(self.gsrt)[1]) * self.interval[-1] / float(len(self.interval)-1)
+        y = np.arange(np.shape(self.gsrt)[1]) * self.interval[-1] / float(
+            len(self.interval) - 1)
         timeskip = self.obj.time_step * self.obj.step_skip
-        x = np.arange(np.shape(self.gsrt)[0]) * self.step_skip * timeskip / 1000.0
-        X, Y = np.meshgrid(x,y, indexing="ij")
+        x = np.arange(
+                np.shape(self.gsrt)[0]) * self.step_skip * timeskip / 1000.0
+        X, Y = np.meshgrid(x, y, indexing="ij")
 
         ticksize = int(figsize[0] * 2.5)
 
@@ -179,20 +191,23 @@ class VanHoveAnalysis(object):
         plt.xlabel("timesteps (ps)", size=labelsize)
         plt.ylabel("r ($\AA$)", size=labelsize)
         plt.axis([x.min(), x.max(), y.min(), y.max()])
-        plt.colorbar(ticks=[0,1]).set_label(label="4$\pi$G$_s$(t,r)", size=labelsize)
+        plt.colorbar(ticks=[0, 1]).set_label(label="4$\pi$G$_s$(t,r)",
+                                             size=labelsize)
         plt.tight_layout()
 
         return plt
 
-    def get_gd_plots(self, figsize=(12,8)):
+    def get_gd_plots(self, figsize=(12, 8)):
         """
         Plot distinct-part van Hove functions.
         """
 
-        y = np.arange(np.shape(self.gdrt)[1]) * self.interval[-1] / float(len(self.interval)-1)
+        y = np.arange(np.shape(self.gdrt)[1]) * self.interval[-1] / float(
+            len(self.interval) - 1)
         timeskip = self.obj.time_step * self.obj.step_skip
-        x = np.arange(np.shape(self.gdrt)[0]) * self.step_skip * timeskip / 1000.0
-        X, Y = np.meshgrid(x,y, indexing="ij")
+        x = np.arange(
+                np.shape(self.gdrt)[0]) * self.step_skip * timeskip / 1000.0
+        X, Y = np.meshgrid(x, y, indexing="ij")
 
         ticksize = int(figsize[0] * 2.5)
 
@@ -206,7 +221,8 @@ class VanHoveAnalysis(object):
         plt.xlabel("timesteps (ps)", size=labelsize)
         plt.ylabel("r ($\AA$)", size=labelsize)
         plt.axis([x.min(), x.max(), y.min(), y.max()])
-        plt.colorbar(ticks=[0,1,2,3,4]).set_label(label="G$_d$(t,r)", size=labelsize)
+        plt.colorbar(ticks=[0, 1, 2, 3, 4]).set_label(label="G$_d$(t,r)",
+                                                      size=labelsize)
         plt.tight_layout()
 
         return plt
