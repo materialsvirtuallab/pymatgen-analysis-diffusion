9000d4cdcc36ce80e0342e04c1d0a48caad54656
zyrd <shyuep@gmail.com>
2019-11-06 08:37:04 -0800

    Remove six usage.

--- setup.py
+++ setup.py
@@ -29,8 +29,6 @@ setup(
     keywords=["VASP", "diffusion", "molecular dynamics", "MD"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
