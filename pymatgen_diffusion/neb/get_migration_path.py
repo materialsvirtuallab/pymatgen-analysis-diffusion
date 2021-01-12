df8061eee729b6dc34f572264e08b9e436edabac
jmmshn <jmmshn@gmail.com>
2021-01-11 23:13:49 -0500

    db specific

---
+++ pymatgen_diffusion/neb/get_migration_path.py
@@ -0,0 +1,293 @@
+from pymatgen import Structure
+import numpy as np
+import logging
+
+from pymatgen.entries.computed_entries import ComputedStructureEntry
+from pymatgen.io.ase import AseAtomsAdaptor
+from ase.build import find_optimal_cell_shape, get_deviation_from_optimal_cell_shape
+from pymatgen.transformations.advanced_transformations import (
+    CubicSupercellTransformation,
+)
+from pymatgen_diffusion.neb.pathfinder import MigrationPath
+from pymatgen_diffusion.neb.full_path_mapper import FullPathMapper, ComputedEntryPath
+from typing import Tuple, List, Union, Dict
+
+"""
+Created on July 21, 2019
+"""
+
+__author__ = "Jimmy Shen"
+__copyright__ = "Copyright 2019, The Materials Project"
+__version__ = "0.1"
+__maintainer__ = "Jimmy Shen"
+__email__ = "jmmshn@lbl.gov"
+__date__ = "April 11, 2019"
+
+logger = logging.getLogger(__name__)
+
+# Commented out for now will delete in the future
+# compat = MaterialsProjectCompatibility("Advanced")
+# # color
+# colors = plt.get_cmap("tab10").colors
+# colors_hex = [rgb2hex(itr) for itr in colors]
+
+
+def get_cep_from_group(
+    base_entry,
+    inserted_entries,
+    working_ion="Li",
+    ltol=0.2,
+    stol=0.3,
+    initial_max_dist=4.0,
+    angle_tol=5,
+    only_use_single=False,
+):
+    """Using a single base_entry and a list of inserted entries, generate an ComputedEntryPath object
+
+    Args:
+        base_entry (ComputedStructureEntry): Base entry for empty structure
+        inserted_entries (list[ComputedStructureEntry]): List of structures with one or more working ions
+        working_ion (str, optional): Name of working_ion. Defaults to 'Li'.
+        ltol (float, optional): Fractional length tolerance. Defaults to 0.2.
+        stol (float, optional): Site tolerance in Angstrom. Defaults to 0.3.
+        angle_tol (int, optional): Angle tolerance in degrees. Defaults to 5.
+
+    Returns:
+        ComputedEntryPath: Object containing all of the symmetry equivalent hops in a system
+    """
+
+    if "aeccar" in base_entry.data.keys():
+        # check this first because cep initi will delete this
+        get_chg_path = True
+    else:
+        get_chg_path = False
+
+    single_inserted = []
+    for entry in inserted_entries:
+        # generate entries from single cation sites
+        num_working = entry.composition.as_dict()[working_ion]
+        if only_use_single and num_working > 1:
+            continue
+        for itr, isite in enumerate(entry.structure.sites):
+            if isite.species_string == working_ion:
+                new_struct = base_entry.structure.copy()
+                new_struct.insert(
+                    0, working_ion, isite.frac_coords, properties=dict(magmom=0)
+                )
+                new_entry = ComputedStructureEntry(
+                    structure=new_struct,
+                    energy=(entry.energy - base_entry.energy) / num_working
+                    + base_entry.energy,
+                    entry_id=str(entry.entry_id) + "_" + str(itr),
+                )
+                single_inserted.append(new_entry)
+    max_dist = initial_max_dist
+    cep = None
+    while max_dist < 10.0:
+        if "aeccar" in base_entry.data.keys():
+            cep = ComputedEntryPath(
+                base_entry,
+                single_cat_entries=single_inserted,
+                migrating_specie=working_ion,
+                base_aeccar=base_entry.data["aeccar"],
+                max_path_length=max_dist,
+                ltol=ltol,
+                stol=stol,
+                angle_tol=angle_tol,
+            )
+            # since the aeccar is already stored we can removed it from the entry
+        else:
+            cep = ComputedEntryPath(
+                base_entry,
+                single_cat_entries=single_inserted,
+                migrating_specie=working_ion,
+                base_aeccar=None,
+                max_path_length=max_dist,
+            )
+        if len(cep.unique_hops) > 0:
+            if "aeccar" in base_entry.data.keys():
+                del cep.base_struct_entry.data["aeccar"]
+            break
+        else:
+            max_dist += 0.2
+
+    if cep is not None and get_chg_path:
+        cep.populate_edges_with_chg_density_info()
+    return cep
+
+
+def get_sc_fromstruct(
+    base_struct: Structure,
+    min_atoms: int = 80,
+    max_atoms: int = 240,
+    min_length: float = 10.0,
+):
+    """
+    Generate the best supercell from a unitcell.
+    The CubicSupercellTransformation from PMG is much faster but don't iterate over as many
+    supercell configurations so it's less able to find the best configuration in a give cell size.
+    We try the PMG version first wit a cap on the number of atoms (max_atoms).
+    If that fails to produce a supercell, we will use the ASE configuration
+    that iterates overall more supercell configurations.
+
+    Args:
+        base_struct: structure of the unit cell
+        max_atoms: Maximum number of atoms allowed in the supercell.
+        min_atoms: Minimum number of atoms allowed in the supercell.
+        min_length: Minimum length of the smallest supercell lattice vector.
+
+    Returns:
+        struc_sc: Supercell that is as close to cubic as possible
+    """
+    struct_sc = _get_sc_from_struct_pmg(base_struct, min_atoms, max_atoms, min_length)
+    if struct_sc is None:
+        logger.warning(
+            "PMG Supercell generation failed, using the SC generation from "
+            "ASE. This might take much longer."
+        )
+        struct_sc = _get_sc_from_struct_ase(base_struct, min_atoms, max_atoms)
+    return struct_sc
+
+
+def _get_sc_from_struct_pmg(
+    base_struct: Structure,
+    min_atoms: int = 80,
+    max_atoms: int = 240,
+    min_length: float = 15.0,
+):
+    """
+    Generate the best supercell from a unitcell using the pymatgen CubicSupercellTransformation
+
+    Args:
+        base_struct: structure of the unit cell
+        max_atoms: Maximum number of atoms allowed in the supercell.
+        min_atoms: Minimum number of atoms allowed in the supercell.
+        min_length: Minimum length of the smallest supercell lattice vector.
+
+
+    Returns:
+
+    """
+    cst = CubicSupercellTransformation(
+        min_atoms=min_atoms, max_atoms=max_atoms, min_length=min_length
+    )
+
+    try:
+        cst.apply_transformation(base_struct)
+    except BaseException:
+        return None
+    return cst.transformation_matrix
+
+
+def _get_sc_from_struct_ase(base_struct, min_atoms=80, max_atoms=240):
+    """generate the best supercell from a unitcell
+
+    Args:
+        base_struct (pymatgen.Structure): unit cell
+        min_size (int, optional): Minimum number of atoms in the desired supercell. Defaults to 80.
+
+    Returns:
+        pymatgen.Structure: supercell
+        3x3 matrix: supercell matrix
+    """
+
+    num_cells_min = int(np.ceil(min_atoms / base_struct.num_sites))
+    num_cells_max = int(np.ceil(max_atoms / base_struct.num_sites))
+    res = []
+    for icell in range(num_cells_min, num_cells_max):
+        atoms = AseAtomsAdaptor().get_atoms(base_struct)
+        logger.info(f"Getting cell shape {icell} x unit cell")
+        sc_mat = find_optimal_cell_shape(atoms.cell, icell, "sc")
+
+        deviation = get_deviation_from_optimal_cell_shape(np.dot(sc_mat, atoms.cell))
+
+        dd = {"deviation": deviation, "P1": sc_mat}
+        # base_struct_sc.to('poscar', f'/Users/lik/Desktop/enpoints_{base_struct_sc.num_sites}.vasp')
+        res.append(dd)
+        if deviation < 0.3:
+            logger.debug("Found good cell")
+            return sc_mat
+
+    else:
+        best_case = sorted(res, key=lambda x: x["deviation"])[0]
+    logger.warning(
+        f"Could not find case with deviation from cubic was less than 0.3 using the ASE cubic supercell finder \
+        \nThe best one had a deviation of {best_case['deviation']}"
+    )
+    return best_case["P1"]
+
+
+def get_start_end_structs_from_hop(
+    hop: MigrationPath, base_struct: Structure, sc_mat: List[List[Union[int, float]]]
+) -> Tuple[Structure, Structure, Structure]:
+    """
+    Obtain the starting and terminating structures in a supercell for NEB calculations.
+
+    Args:
+        hop: object presenting the migration event
+        base_struct: unit cell representation of the structure
+        sc_mat: supercell transformation to create the simulation cell for the NEB calc
+
+    Returns:
+        initial structure, final structure, empty structure all in the supercell
+    """
+    base_sc = base_struct.copy() * sc_mat
+
+    start_struct = base_struct.copy() * sc_mat
+    end_struct = base_struct.copy() * sc_mat
+
+    sc_mat_inv = np.linalg.inv(sc_mat)
+
+    start_struct.insert(
+        0,
+        hop.esite.species_string,
+        np.dot(hop.isite.frac_coords, sc_mat_inv),
+        properties={"magmom": 0},
+    )
+    end_struct.insert(
+        0,
+        hop.esite.species_string,
+        np.dot(hop.esite.frac_coords, sc_mat_inv),
+        properties={"magmom": 0},
+    )
+    return start_struct, end_struct, base_sc
+
+
+def get_start_end_structs(
+    fpm: FullPathMapper,
+    min_length: float = 10.0,
+    store_base=True,
+    min_atoms: int = 80,
+    max_atoms: int = 240,
+) -> Dict:
+    """
+    Iterate through the unique hops of a FullPathMapper object to generate the list of
+    initial and final NEB calculation positions
+
+    Args:
+        fpm:  Migration graph
+        min_length: smalled side legth
+        store_base: if True return the base structure
+        min_atoms: minimum number of atoms in the base cell
+        max_atoms:  maximum number of atoms in the base cell (make sure this is not
+        too strict)
+    Returns:
+        Dict: { unique hop label :
+            {'start': start structure, 'end': end structure, 'base': empty structure}
+        }
+    """
+    base = Structure.from_sites([isite for isite in fpm.symm_structure.copy()])
+    base.remove_species([fpm.migrating_specie])
+    sc_mat = get_sc_fromstruct(
+        base, min_atoms=min_atoms, max_atoms=max_atoms, min_length=min_length
+    )
+    res = dict()
+    for k, uniq_hop in fpm.unique_hops.items():
+        start_struct, end_struct, base_struct = get_start_end_structs_from_hop(
+            uniq_hop["hop"], base, sc_mat
+        )
+        if store_base:
+            res[k] = {"start": start_struct, "end": end_struct, "base": base_struct}
+        else:
+            res[k] = {"start": start_struct, "end": end_struct}
+    return res
---
+++ pymatgen_diffusion/utils/tests/test_get_db_data.py
@@ -0,0 +1,113 @@
+# coding: utf-8
+# Copyright (c) Materials Virtual Lab.
+# Distributed under the terms of the BSD License.
+from pymatgen.entries.computed_entries import ComputedStructureEntry
+
+from pymatgen.analysis.structure_matcher import StructureMatcher
+import unittest
+from pymatgen import Structure
+import numpy as np
+import os
+
+from pymatgen_diffusion.utils.get_db_data import get_inserted_on_base, process_ents
+
+dir_path = os.path.dirname(os.path.realpath(__file__)) + "/../../neb/tests/"
+
+__author__ = "Jimmy Shen"
+__version__ = "1.0"
+__date__ = "April 10, 2019"
+
+
+class GetDbDataSimplTest(unittest.TestCase):
+    def setUp(self):
+        struct_uc = Structure.from_dict(
+            {
+                "@module": "pymatgen.core.structure",
+                "@class": "Structure",
+                "charge": None,
+                "lattice": {
+                    "matrix": [[3, 0, 0], [1.5, 2.598, 0], [0, 0, 6]],
+                    "a": 3,
+                    "b": 3,
+                    "c": 6,
+                    "alpha": 90,
+                    "beta": 90,
+                    "gamma": 60,
+                    "volume": 46.77,
+                },
+                "sites": [
+                    {
+                        "species": [{"element": "B", "occu": 1}],
+                        "abc": [0, 0, 0.5],
+                        "xyz": [0, 0, 3],
+                        "label": "B",
+                        "properties": {},
+                    },
+                    {
+                        "species": [{"element": "N", "occu": 1}],
+                        "abc": [0.5, 0.5, 0.5],
+                        "xyz": [2.25, 1.299, 3],
+                        "label": "N",
+                        "properties": {},
+                    },
+                ],
+            }
+        )
+
+        e_uc = 100
+        self.base = ComputedStructureEntry(structure=struct_uc, energy=e_uc)
+
+        sc = struct_uc * [2, 2, 2]
+        sc.insert(0, "Li", [0.125, 0.125, 0.25])
+        self.inserted_1Li1 = ComputedStructureEntry(structure=sc, energy=e_uc * 8 + 3)
+
+        sc = struct_uc * [2, 2, 2]
+        sc.insert(0, "Li", [0.375, 0.375, 0.25])
+        self.inserted_1Li2 = ComputedStructureEntry(structure=sc, energy=e_uc * 8 + 5)
+
+        sc = struct_uc * [2, 2, 2]
+        sc.insert(0, "Li", [0.125, 0.125, 0.25])
+        sc.insert(0, "Li", [0.375, 0.375, 0.25])
+        self.inserted_2Li = ComputedStructureEntry(structure=sc, energy=e_uc * 8 + 4)
+
+        self.sm = StructureMatcher(ignored_species=["Li"], primitive_cell=False)
+
+    def _is_valid_inserted_ent(self, mapped_ents):
+        res = True
+        for ient in mapped_ents:
+            for isite in ient.structure.sites:
+                if isite.species_string == "Li":
+                    tt = isite.frac_coords - np.floor(isite.frac_coords)
+                    if list(tt) in [[0.75, 0.75, 0.5], [0.25, 0.25, 0.5]]:
+                        res = True
+                    else:
+                        return False
+        return res
+
+    def test_get_inserted_on_base(self):
+        mapped_ents = get_inserted_on_base(self.base, self.inserted_1Li1, self.sm)
+        self.assertTrue(self._is_valid_inserted_ent(mapped_ents))
+        self.assertEqual(mapped_ents[0].energy, 103)
+        mapped_ents = get_inserted_on_base(self.base, self.inserted_1Li2, self.sm)
+        self.assertTrue(self._is_valid_inserted_ent(mapped_ents))
+        self.assertEqual(mapped_ents[0].energy, 105)
+        mapped_ents = get_inserted_on_base(self.base, self.inserted_2Li, self.sm)
+        self.assertTrue(self._is_valid_inserted_ent(mapped_ents))
+        self.assertEqual(mapped_ents[0].energy, 102)
+
+    def test_process_ents(self):
+        base_2_ent = ComputedStructureEntry(
+            structure=self.base.structure * [[1, 1, 0], [1, -1, 0], [0, 0, 2]],
+            energy=self.base.energy * 4,
+        )
+        res = process_ents(
+            [base_2_ent, self.base], [self.inserted_1Li1, self.inserted_2Li]
+        )
+        for itr_group in res:
+            base_energy = itr_group["base"].energy
+            for i_insert in itr_group["inserted"]:
+                self.assertTrue(i_insert.energy - base_energy in [2, 3])
+
+
+if __name__ == "__main__":
+    unittest.main()
