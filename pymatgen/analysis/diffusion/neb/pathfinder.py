4a1a7db759b760e00524e1eec61baac5a1ea68e1
Shyue Ping Ong <sp@ong.ai>
2022-07-21 10:50:51 -0700

    Fix mypy.

--- pymatgen/analysis/diffusion/neb/pathfinder.py
+++ pymatgen/analysis/diffusion/neb/pathfinder.py
@@ -324,7 +324,7 @@ class MigrationHop(MSONable):
         self.symprec = symprec
         self.msite = PeriodicSite(esite.specie, (isite.frac_coords + esite.frac_coords) / 2, esite.lattice)
         if host_symm_struct:
-            sg = self.host_symm_struct.spacegroup
+            sg = self.host_symm_struct.spacegroup  # type: ignore
         else:
             sg = self.symm_structure.spacegroup
         for i, sites in enumerate(self.symm_structure.equivalent_sites):
