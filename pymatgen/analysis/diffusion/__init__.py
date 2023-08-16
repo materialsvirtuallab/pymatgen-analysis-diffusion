f072438cf7c095680bb5ad2b5f54dd45c1df0e10
Shyue Ping Ong <sp@ong.ai>
2023-08-15 21:00:16 -0700

    Update docs.

--- pymatgen/analysis/diffusion/__init__.py
+++ pymatgen/analysis/diffusion/__init__.py
@@ -1,7 +1,8 @@
 """
 Package for diffusion analysis.
 """
+from __future__ import annotations
 
 __author__ = "Materials Virtual Lab"
 __email__ = "ongsp@eng.ucsd.edu"
-__version__ = "2022.7.21"
+__version__ = "2023.8.15"
